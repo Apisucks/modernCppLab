5a6dc70465e83c2a76c31d70cdd2c513c2b0ef09
Apisucks <aman_deep04@live.com>
2021-04-06 19:32:44 +0530

    Dynamic cast example added

---
+++ Code_Examples/CastExamples.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class A{
+    public:
+        virtual void print(){ cout << __PRETTY_FUNCTION__ << endl;}
+};
+
+class B: public A {
+    public:
+        void print(){
+            cout << __PRETTY_FUNCTION__ << endl;
+        }
+};
+
+class C: public A {
+    
+};
+
+int main(){
+
+    A *ab = new B();
+
+    B *b = dynamic_cast<B*>(ab);
+    if (b){
+        cout << "Casting B -> B successful" << endl;
+    }
+
+    A *ac = new C();
+    B *bc = dynamic_cast<B*>(ac);
+    if (bc){
+        cout << "Casting C -> B successful" << endl;
+    }
+    return 0;
+}
